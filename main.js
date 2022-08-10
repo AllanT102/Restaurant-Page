cf65097eebe3cda1c0b8a8933bd43e1f67a9dfe6
AllanT102 <allan.zf.tan@gmail.com>
2022-08-09 21:30:36 -0700

    Initial asset/output management complete

---
+++ main.js
@@ -0,0 +1,19 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./src/components/index.js ***!
+  \*********************************/
+function component() {
+    const content = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = "/src/images/test.jpg";
+    const text = document.createElement('p');
+    text.textContent = "this restaurant is amazing";
+    content.append(img, text);
+    return content;
+}
+
+document.body.appendChild(component());
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsdUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50X3BhZ2UvLi9zcmMvY29tcG9uZW50cy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiBjb21wb25lbnQoKSB7XHJcbiAgICBjb25zdCBjb250ZW50ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnZGl2Jyk7XHJcbiAgICBjb25zdCBpbWcgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdpbWcnKTtcclxuICAgIGltZy5zcmMgPSBcIi9zcmMvaW1hZ2VzL3Rlc3QuanBnXCI7XHJcbiAgICBjb25zdCB0ZXh0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgncCcpO1xyXG4gICAgdGV4dC50ZXh0Q29udGVudCA9IFwidGhpcyByZXN0YXVyYW50IGlzIGFtYXppbmdcIjtcclxuICAgIGNvbnRlbnQuYXBwZW5kKGltZywgdGV4dCk7XHJcbiAgICByZXR1cm4gY29udGVudDtcclxufVxyXG5cclxuZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChjb21wb25lbnQoKSk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
