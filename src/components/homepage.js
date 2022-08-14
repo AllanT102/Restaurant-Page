6cc3936b57c1c5dfdf260e3b4d70f2a1e1dbf81e
AllanT102 <allan.zf.tan@gmail.com>
2022-08-13 18:23:35 -0700

    divs show up on scroll, but need to fix timing

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -6,8 +6,13 @@ let content;
 
 export default function homepage() {
     content = document.createElement('div');
-    content.append(aboutSection());
+    content.append(aboutSection(), reviewsSection(), bestMenuSection(), bestMenuSection(), bestMenuSection());
     content.classList.add('content')
+
+    const children = content.children;
+    for (let i = 0; i < children.length; i++) {
+        i % 2 === 0 ? children[i].classList.add('right') : children[i].classList.add('left');
+    }
     return content;
 };
 
@@ -18,13 +23,13 @@ function aboutSection() {
     icon.src = aboutIcon;
 
     const text = document.createElement('p');
-    text.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    text.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
     section.classList.add('about-section')
     icon.classList.add('section-icon')
     text.classList.add('section-text')
     setTimeout(() => {
-        section.classList.add('active')
+        section.classList.add('active-right')
     }, 200)
 
     section.append(text, icon);
@@ -33,20 +38,44 @@ function aboutSection() {
 
 function reviewsSection() {
     const section = document.createElement('div');
+
+    const text = document.createElement('p');
+    text.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    
+    section.classList.add('review-section')
+    text.classList.add('section-text');
+
+    section.append(text);
+    return section;
 }
 
 function bestMenuSection() {
     const section = document.createElement('div');
+
+    const text = document.createElement('p');
+    text.textContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    
+    section.classList.add('review-section')
+    text.classList.add('section-text');
+
+    section.append(text);
+    return section;
 }
 
 function reveal() {
     const children = content.children;
-    for (const child of children) {
+    console.log(children.length);
+    for (let i = 1; i < children.length; i++) {
+        let child = children[i];
+        if (child === content.firstChild) continue;
         let height = child.getBoundingClientRect().top;
-        if (height < 500) child.classList.add('active');
+        if (height < 900 && i % 2 === 0) child.classList.add('active-right');
+        else if (height < 900 && i % 2 !== 0) child.classList.add('active-left');
+        else child.classList.remove('active-left');
     }
 }
 
 window.addEventListener('scroll', reveal);
+window.addEventListener('load', reveal);
 
 
